c11de8f459a87f7365190dcbf6081ebd8fd02972
Leesa Ward <leesa@doubleedesign.com.au>
2022-12-10 18:37:30 +1100

    Deployment stuff

--- server/app.js
+++ server/app.js
@@ -3,12 +3,12 @@ import libraryRoutes from './itunes-library/routes.js';
 import setlistRoutes from './setlist/routes.js';
 
 const app = express();
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/library', libraryRoutes);
 app.use('/setlist', setlistRoutes);
 
-app.listen(4000, () => {
-	console.log('Server running on port 4000');
+app.listen(8080, () => {
+	console.log('Server running on port 8080');
 });
