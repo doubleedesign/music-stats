ba6535f0950a3ae5b7a5061f55ce67be3f2fe4fb
Leesa Ward <leesa@doubleedesign.com.au>
2022-12-10 23:32:55 +1100

    feat(setlist): use JSON for saving setlist records locally only, not in prod

--- server/setlist/setlistfm.js
+++ server/setlist/setlistfm.js
@@ -4,60 +4,63 @@ import { CosmosClient } from '@azure/cosmos';
 const api = 'https://api.setlist.fm/rest/1.0';
 import savedArtists from '../data/setlist_artists.json' assert { type: 'json' }; // TODO: Get this from the database and use the JSON for local dev and testing only
 import * as dotenv from 'dotenv';
+import express from 'express';
 dotenv.config();
+
+const app = express();
+const environment = app.get('env').trim();
 
 const setlistfm = {
 	/**
 	 * Get Setlist.fm artist object (mainly for the mbid value, to be used for further Setlist queries)
-	 * If the artist is already in locally saved file, return that
+	 * If the artist is already in locally saved file, return that // TODO: Or the database
 	 * If they aren't, query the Setlist API and save + return the result
 	 * @param name
 	 * @return {Promise<{data: (*|string), status: (*|number)}|{data: *, status: number}>}
 	 */
 	getArtist: async function (name) {
 		const url = `${api}/search/artists?artistName=${encodeURIComponent(name)}&p=1&sort=sortName`;
+		const saved = savedArtists.find(item => item.name === name);
 		let result;
 
-		// TODO: Update this to query the database
-		const saved = savedArtists.find(item => item.name === name);
-		if (saved) {
-			result = {
+		if(environment === 'LOCAL' && saved) {
+			return {
 				response: 200,
 				data: saved
 			};
+		}
+
+		// TODO: Update this to get saved value from the database in prod or if there isn't one locally
+		
+		try {
+			const response = await axios.get(url, {
+				headers: {
+					'Accept': 'application/json',
+					'Accept-Encoding': 'gzip, deflate, br',
+					'x-api-key': process.env.SETLIST_API_KEY
+				}
+			});
+			const searchResults = response.data.artist;
+			result = {
+				status: 200,
+				data: searchResults.find(item => {
+					return item.name === name; // return the likely correct result if there's multiple
+				})
+			};
+
+			await this.saveArtist(result.data);
+
 			return result;
 		}
-		else {
-			try {
-				const response = await axios.get(url, {
-					headers: {
-						'Accept': 'application/json',
-						'Accept-Encoding': 'gzip, deflate, br',
-						// eslint-disable-next-line no-undef
-						'x-api-key': process.env.SETLIST_API_KEY
-					}
-				});
-				const searchResults = response.data.artist;
-				result = {
-					status: 200,
-					data: searchResults.find(item => {
-						return item.name === name; // return the likely correct result if there's multiple
-					})
-				};
-
-				await this.saveArtist(result.data);
-
-				return result;
-			}
-			catch (error) {
-				console.log(error);
-				result = {
-					status: error?.response?.status || 500,
-					data: error?.response?.data || 'Error in setlistfm.js'
-				};
-				return result;
-			}
+		catch (error) {
+			console.log(error);
+			result = {
+				status: error?.response?.status || 500,
+				data: error?.response?.data || 'Error in setlistfm.js'
+			};
+			return result;
 		}
+
 	},
 
 	/**
@@ -78,11 +81,12 @@ const setlistfm = {
 		await container.items.create(data);
 
 		// Save to file for local use
-		// TODO: Only do this in local environment
-		const jsonString = fs.readFileSync('./data/setlist_artists.json');
-		const jsonObject = JSON.parse(jsonString);
-		jsonObject.push(data);
-		fs.writeFileSync('./data/setlist_artists.json', JSON.stringify(jsonObject, null, 4));
+		if(environment === 'LOCAL') {
+			const jsonString = fs.readFileSync('./data/setlist_artists.json');
+			const jsonObject = JSON.parse(jsonString);
+			jsonObject.push(data);
+			fs.writeFileSync('./data/setlist_artists.json', JSON.stringify(jsonObject, null, 4));
+		}
 	},
 
 	/**
